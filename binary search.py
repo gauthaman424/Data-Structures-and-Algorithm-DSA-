b9fab38cc7763d0f8b6ae78d70a7efd06ec6dff4
GAUTHAMAN <sgntvm@gmail.com>
2024-05-18 11:56:03 +0530

    changed variable name in binary search

--- binary search.py
+++ binary search.py
@@ -1,15 +1,15 @@
-doomsday=[11, 12, 25, 45, 85, 99, 169, 189]
+number_list=[11, 12, 25, 45, 85, 99, 169, 189]
 t=189
 start=0
-end=len(doomsday)-1
-mid=len(doomsday)//2
+end=len(number_list)-1
+mid=len(number_list)//2
 while mid > 0 and mid <= end:
-    if doomsday[mid]==t:
+    if number_list[mid]==t:
         print("found", mid)
         break
-    elif t < doomsday[mid]:
+    elif t < number_list[mid]:
         end = mid -1
         mid = (start + end)//2 
-    elif t > doomsday[mid]:
+    elif t > number_list[mid]:
         start = mid + 1
         mid = (start + end)//2 
